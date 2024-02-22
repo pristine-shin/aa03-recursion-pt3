72165aa670b07fc6e9c3805e4f569c12f9481159
Pristine <prisvsyo@gmail.com>
2024-02-22 15:21:57 -0800

    90% completed

--- problems/09-fibonacci.js
+++ problems/09-fibonacci.js
@@ -5,7 +5,8 @@ and returns the `n`th number in the Fibonacci sequence.
 Not familiar with the Fibonacci sequence? Beginning with 0 and 1, we add the two
 previous numbers in the sequence together to form the next one:
 
-0, 1, 1, 2, 3, 5, 8, etc....
+(0), 1, 1, 2, 3, 5, 8, etc....
+n == 1, 2, 3, 4, 5, 6, ...
 
 We count Fibonacci numbers beginning with the first 1. Take a look at the
 examples below if you're unsure where to start!
@@ -20,9 +21,19 @@ fibonacci(10); // 55
 ***********************************************************************/
 
 function fibonacci(n) {
-  // Your code here 
+  if (n < 3) {
+    return 1;
+  }
+
+  return fibonacci(n - 1) + fibonacci(n - 2);
+
 }
-  
+
+console.log(fibonacci(1)); // 1
+console.log(fibonacci(2)); // 1
+console.log(fibonacci(3)); // 2
+console.log(fibonacci(4)); // 3
+console.log(fibonacci(10)); // 55
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = fibonacci;
-  
--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -7,15 +7,34 @@ Do not use the built in Array.flat.
 
 Examples:
 
-flatten([]); // []
-flatten([1, 2]); // [1, 2]
-flatten([1, [2, [3]]]); // [1, 2, 3]
+console.log(flatten([])); // []
+console.log(flatten([1, 2])); // [1, 2]
+console.log(flatten([1, [2, [3]]])); // [1, 2, 3]
+console.log(flatten([[1, 2], [3, [4, 5]]])) //[1, 2, 3, 4, 5]
+console.log(flatten([[[[]]]]), []) // [];
+
+i want to destructure each time?
 ***********************************************************************/
 
 function flatten(arr) {
-  // Your code here 
+  if (arr.length === 0) {
+    return arr
+  }
+  if (!Array.isArray(arr[0])) {
+    return [arr[0], ...flatten(arr.slice(1))];
+  } else {
+    return [...flatten(arr[0]), ...flatten(arr.slice(1))]
+  }
 }
-  
+
+// let arr = [[1, 2], [3, [4, 5]]]
+// console.log(...arr)
+
+console.log(flatten([])); // []
+console.log(flatten([1, 2])); // [1, 2]
+console.log(flatten([1, [2, [3]]])); // [1, 2, 3]
+console.log(flatten([[1, 2], [3, [4, 5]]])) //[1, 2, 3, 4, 5]
+console.log(flatten([[[[]]]]), []) // [];
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = flatten;
-  
